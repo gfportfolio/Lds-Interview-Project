97e63d592f33efc382405b37fbf492f842f899c4
Gavin Farnsworth <gbfarns16@gmail.com>
2018-04-16 21:59:23 -0600

    Setup Video

---
+++ src/lds-article/lds-article-video.js
@@ -0,0 +1,12 @@
+/**
+ * @customElement
+ * @polymer
+ */
+class LdsArticleVideo extends Polymer.Element {
+  static get is() {
+    return 'lds-article-video';
+  }
+
+}
+
+window.customElements.define(LdsArticleVideo.is, LdsArticleVideo);
